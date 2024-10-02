4844504530b185cd87fd0a55e97fa71b328ac464
Evina88 <evinamous@gmail.com>
2024-10-02 13:11:14 +0300

    added courses table

--- resources/views/course/index.blade.php
+++ resources/views/course/index.blade.php
@@ -5,5 +5,24 @@
 <h1>
     Courses
 </h1>
-
+<table>
+<thead>
+    <tr>
+        <th>Course Name</th>
+        <th>Instructor</th>
+    </tr>
+</thead>
+<tbody>
+    @foreach ($courses as $course)
+    <tr>
+        <td>
+            {{$course -> title}}
+        </td>
+        <td>
+            {{$course -> instructor}}
+        </td>
+    </tr>
+    @endforeach
+</tbody>
+</table>
 @endsection
